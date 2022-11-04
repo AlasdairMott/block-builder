8396c495639638e491b876c96675cf3dfd1788bb
Alasdair Mott <alasdair.c.mott@gmail.com>
2022-11-05 09:05:30 +1100

    Update HelpAndSound.tsx

--- src/UI/HelpAndSound.tsx
+++ src/UI/HelpAndSound.tsx
@@ -1,4 +1,5 @@
 import { Cancel, EmptyPage, Erase, Home, PerspectiveView, QuestionMark, Redo, SoundHigh, SoundOff, Svg3DSelectSolid, Undo } from "iconoir-react";
+import { useCallback, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { toggleSound } from "../store/ui";
 import RoundButton from "../UI/RoundButton";
@@ -22,6 +23,23 @@ const HelpAndSoundButtons = (props: { onShowHelp: () => void }) => {
 }
 
 const HelpModal = (props: { onClose: () => void }) => {
+
+    const handleKeyPress = useCallback((event: any) => {
+        if (event.key === 'Escape') {
+            props.onClose();
+        }
+    }, [props]);
+
+    useEffect(() => {
+        // attach the event listener
+        document.addEventListener('keydown', handleKeyPress);
+
+        // remove the event listener
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleKeyPress]);
+
     return (
         <div className={styles.helpModal} onClick={props.onClose}>
             <div className={styles.helpModal__content}>
@@ -59,8 +77,8 @@ const HelpModal = (props: { onClose: () => void }) => {
                 </table>
                 <br />
                 <p>Left and right arrow keys rotate the block.</p>
-                <p>Up arrow to changes the block.</p>
-                <p>Down arrow to changes the block colour.</p>
+                <p>Up arrow changes the block.</p>
+                <p>Down arrow changes the block colour.</p>
                 <button className={styles.helpModalButton} onClick={props.onClose} title={"Close"}>
                     <Cancel {...ICONPROPS} />
                 </button>
