8e704f4fceb0293e166d04ed16508ee5598a35ad
FedericoDiTosto <federico.ditosto@gmail.com>
2022-11-06 21:28:49 +0100

    added gmail

--- src/UI/ShareModal.tsx
+++ src/UI/ShareModal.tsx
@@ -1,4 +1,4 @@
-import { MessageText, Telegram, Twitter, Pinterest, PasteClipboard, Cancel, CheckCircledOutline } from "iconoir-react";
+import { MessageText, Telegram, Twitter, Pinterest, Copy, Mail, Cancel, CheckCircledOutline } from "iconoir-react";
 import { useCallback, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { toggleSound } from "../store/ui";
@@ -30,10 +30,10 @@ const ShareModal = (props: { onClose: () => void }) => {
                 setTimeout(function () {
                     setCopiedClipboard(false)
                     props.onClose()
-                }, 100000);
+                }, 1000);
                 break;
             //list social url : https://crunchify.com/list-of-all-social-sharing-urls-for-handy-reference-social-media-sharing-buttons-without-javascript/
-            case "Whatsapp":
+            case "WhatsApp":
                 window.open("https://api.whatsapp.com/send?text=" + urlString)
                 props.onClose()
                 break;
@@ -45,6 +45,10 @@ const ShareModal = (props: { onClose: () => void }) => {
                 window.open("https://twitter.com/share?url=" + urlString)
                 props.onClose()
                 break;
+            case "Gmail":
+                window.open("https://mail.google.com/mail/?view=cm&fs=1&tf=1&body=" + urlString + "+'&ui=2&tf=1&pli=1';")
+                props.onClose()
+                break
             case "Pinterest":
                 window.open("https://pinterest.com/pin/create/bookmarklet/?url=" + urlString)
                 props.onClose()
@@ -58,10 +62,11 @@ const ShareModal = (props: { onClose: () => void }) => {
                 {copiedClipboard
                     ? <div className={styles.copiedClipboard}><CheckCircledOutline color="green" strokeWidth="1px" /> Copied to the clipboard</div>
                     : <div className={styles.buttonsContainer}>
-                        <div> <ShareModalButton onClick={() => handleShare("Clipboard")} title={"Clipboard"}> <PasteClipboard {...SHAREICONPROPS} /></ShareModalButton></div>
-                        <div><ShareModalButton onClick={() => handleShare("Whatsapp")} title={"Whatsapp"}> <MessageText {...SHAREICONPROPS} /> </ShareModalButton></div>
+                        <div> <ShareModalButton onClick={() => handleShare("Clipboard")} title={"Copy link"}> <Copy {...SHAREICONPROPS} /></ShareModalButton></div>
+                        <div><ShareModalButton onClick={() => handleShare("WhatsApp")} title={"WhatsApp"}> <MessageText {...SHAREICONPROPS} /> </ShareModalButton></div>
                         <div><ShareModalButton onClick={() => handleShare("Telegram")} title={"Telegram"}> <Telegram {...SHAREICONPROPS} /> </ShareModalButton></div>
                         <div><ShareModalButton onClick={() => handleShare("Twitter")} title={"Twitter"}> <Twitter {...SHAREICONPROPS} /> </ShareModalButton></div>
+                        <div><ShareModalButton onClick={() => handleShare("Gmail")} title={"Gmail"} > <Mail {...SHAREICONPROPS} /> </ShareModalButton></div>
                         <div><ShareModalButton onClick={() => handleShare("Pinterest")} title={"Pinterest"}> <Pinterest {...SHAREICONPROPS} /> </ShareModalButton></div>
                     </div>
                 }
